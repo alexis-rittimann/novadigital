3b744916deb6349f15136a165d67fa727935fa48
alexis <alexritti111@gmail.com>
2021-02-10 10:15:30 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,140 @@
+let controller;
+let slideScene;
+let pageScene;
+let detailScene;
+
+const mouse = document.querySelector('.cursor');
+const mouseTxt = mouse.querySelector('span');
+const burger = document.querySelector(".burger");
+const menu = document.querySelector(".nav-bar");
+const img = document.querySelectorAll(".hero-img")
+
+
+burger.addEventListener("click", navToggle);
+
+function cursor(e){
+    mouse.style.top = e.pageY + "px";
+    mouse.style.left = e.pageX + "px";
+}
+function activeCursor(e){
+  const item = e.target;
+  if (item.id === 'logo' || item.classList.contains('burger')) {
+    mouse.classList.add('nav-active');
+  }else{
+    mouse.classList.remove('nav-active');
+  }
+  if (item.classList.contains('explore')) {
+    mouse.classList.add('explore-active');
+    gsap.to('.title-swipe', 1, { y: "0%"});
+    mouseTxt.innerText = "Tap";
+  }else{
+    mouse.classList.remove("explore-active");
+    mouseTxt.innerText = "";
+    gsap.to(".title-swipe", 0.6, { y: "100%" });
+  }
+}
+function navToggle(e){
+    if (!e.target.classList.contains("active")) {
+    e.target.classList.add('active');
+    gsap.to('.line1', 0.5, {rotate: "45", y: 5, background:"black"});
+    gsap.to('.line2', 0.5, {rotate: "-45", y: -5, background:"black"});
+    gsap.to("#logo", 1, { color: "black" });
+    gsap.to('.nav-bar', 1, {clipPath: "circle(2500px at 100% -10%)"});
+    document.body.classList.add("hide");
+    menu.style.height = "100%";
+    menu.style.width = "100%";
+    mouse.style.border = " 2px solid black";
+    }else{
+    e.target.classList.remove('active');
+    gsap.to('.line1', 0.5, {rotate: "0", y: 0, background:"white"});
+    gsap.to('.line2', 0.5, {rotate: "0", y: 0, background:"white"});
+    gsap.to("#logo", 1, { color: "white" });
+    gsap.to('.nav-bar', 1, {clipPath: "circle(50px at 100% -10%)"});
+    document.body.classList.remove("hide");
+    menu.style.height = "0%";
+    menu.style.width = "0%";
+    mouse.style.border = " 2px solid white";
+    }
+  }
+  // //Barba Page Transitions
+  // const logo = document.querySelector("#logo");
+  // barba.init({
+  //   views: [
+  //     {
+  //       namespace: "home",
+  //       beforeEnter() {
+  //         animateSlides();
+  //         logo.href = "./index.html";
+  //       },
+  //       beforeLeave() {
+  //         slideScene.destroy();
+  //         pageScene.destroy();
+  //         controller.destroy();
+  //
+  //       }
+  //     },
+  //     {
+  //       namespace: "fashion",
+  //       beforeEnter() {
+  //         logo.href = "../index.html";
+  //         detailAnimation();
+  //         location.reload();
+  //
+  //       },
+  //       beforeLeave() {
+  //         controller.destroy();
+  //         detailScene.destroy();
+  //
+  //       }
+  //     }
+  //   ],
+  //   transitions: [
+  //     {
+  //       leave({ current, next }) {
+  //         let done = this.async();
+  //         //An Animation
+  //         const tl = gsap.timeline({ defaults: { ease: "power2.inOut" } });
+  //         tl.fromTo(current.container, 1, { opacity: 1 }, { opacity: 0 });
+  //         tl.fromTo(
+  //           ".swipe",
+  //           0.75,
+  //           { x: "-100%" },
+  //           { x: "0%", onComplete: done },
+  //           "-=0.5"
+  //         );
+  //       },
+  //       enter({ current, next }) {
+  //         let done = this.async();
+  //         //Scroll to the top
+  //         window.scrollTo(0, 0);
+  //         //An Animation
+  //         const tl = gsap.timeline({ defaults: { ease: "power2.inOut" } });
+  //         tl.fromTo(
+  //           ".swipe",
+  //           1,
+  //           { x: "0%" },
+  //
+  //           { x: "100%", stagger: 0.2, onComplete: done }
+  //         );
+  //         tl.fromTo(next.container, 1, { opacity: 0 }, { opacity: 1 });
+  //
+  //         tl.fromTo(
+  //           ".nav-header",
+  //           1,
+  //           { y: "-100%" },
+  //           { y: "0%", ease: "power2.inOut" },
+  //           "-=1.5"
+  //         );
+  //       }
+  //     }
+  //   ]
+  //
+  // });
+
+
+
+
+
+
+  window.addEventListener('mousemove', cursor);
+  window.addEventListener('mouseover', activeCursor);
